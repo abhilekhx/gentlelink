9eaaba4f139c41056ecd45223184522e2a4e0984
abhilekhx <lekh7adhikari@gmail.com>
2025-10-15 22:00:41 -0400

    added materialui

--- app/page.js
+++ app/page.js
@@ -1,10 +1,20 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import AddReactionIcon from "@mui/icons-material/AddReaction";
+import AddCallIcon from "@mui/icons-material/AddCall";
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World from GentleTech</h1>
-    </div>
+    <Stack spacing={2} direction="row">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <Button color="secondary">Secondary</Button>
+      <Button variant="contained" color="success">
+        Success
+      </Button>
+      <Button variant="outlined" color="error">
+        Error
+      </Button>
+    </Stack>
   );
 }
